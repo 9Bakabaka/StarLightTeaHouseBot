4cdf0da89dd340cf74bcd366dc236a2d3cee921c
Longtail <endermen359872@icloud.com>
2025-04-02 19:24:47 +0800

    Reformatted LLM.py

--- LLM.py
+++ LLM.py
@@ -7,34 +7,45 @@ with open('DSapi.txt', 'r') as f:
 
 # platform: "DS" "GPT"
 def fetch_from_AI(platform, sys_prompt, user_prompt):
+    print("Start fetching from AI")
+    AI_model = ""
     if platform == "DS":
         base_url = "https://api.deepseek.com"
+        AI_model = "deepseek-chat"
     elif platform == "GPT":
-        base_url = "https://api.openai.com"
+        # base_url = "https://api.openai.com"
+        base_url = "https://api.f2gpt.com"
+        AI_model = "gpt-3.5-turbo"
     else:
-        return "Error: Invalid platform."
+        print("Error: Invalid platform")
+        return "Error"
 
     response = None
     client = OpenAI(api_key=DS_api, base_url=base_url)
     try:
         response = client.chat.completions.create(
-            model="deepseek-chat",
+            model=AI_model,
             messages=[
                 {"role": "system", "content": sys_prompt},
                 {"role": "user", "content": user_prompt},
             ],
             stream=False
         )
-    except APIStatusError as e:
+    except APIStatusError as e: # handle APIStatusError
         if e.status_code == 402:
             print("Insufficient Balance")
-            return e
         else:
             print(f"APIStatusError: {e}")
-            return e
+        return "Error"
+
+    except Exception as e:  # handle other exceptions
+        print(f"Error: {e}")
+        return "Error"
 
     if response:
-        print(response.choices[0].message.content)
+        print("AI response: ", response.choices[0].message.content)
         return response.choices[0].message.content
+
+    return "Error"
 
 # if excepting model "remember context", user_prompt = last_user_prompt + this_user_prompt
