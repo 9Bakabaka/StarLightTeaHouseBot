ba4c584cb4c4f4dc16e50ff622d984353f3b58a1
Longtail <endermen359872@icloud.com>
2025-04-13 10:15:09 +0800

    Bugs fixed

--- main.py
+++ main.py
@@ -683,6 +683,30 @@ async def denno_mienmien_mao_nonnon(update: Update, context: ContextTypes.DEFAUL
     print(datetime.datetime.now(), "\t", "Received 电脑眠眠猫.")
     await context.bot.send_message(chat_id=update.effective_chat.id, text="\\non non!/")
 
+# backdoor to send message in any group
+# aha not a real back door
+async def backdoor(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(datetime.datetime.now(), "\t", "Received /bd.")
+    # only Longtail can use this backdoor
+    if not update.message.from_user.id == 5418690874:   # 5418690874: Longtail, change as you wish
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="You are not my master. I would refuse your request.")
+        return
+    if not re.match(r'^/bd -\d+ .*', update.message.text):
+        print(datetime.datetime.now(), "\t", "Sending usage.")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Format not correct.")
+        return
+    # usage: /bd group_id message
+    group_id = update.message.text.split(' ')[1]
+    message = update.message.text.split(' ')[2]
+    try:
+        print(datetime.datetime.now(), "\t", "Try sending ", message, " to ", group_id)
+        send_to_chat = await context.bot.get_chat(chat_id=group_id)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Try sending\n" + message + "\nto\n" + send_to_chat.title)
+        await context.bot.send_message(chat_id=send_to_chat.id, text=message)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Success.")
+    except Exception as e:
+        print(datetime.datetime.now(), "\t", "Error: ", e)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Error: " + str(e))
 
 def main():
     application = ApplicationBuilder().token(botToken).build()
@@ -783,6 +807,9 @@ def main():
 
     un_xm_handler = CommandHandler('unxm', un_xm)
     application.add_handler(un_xm_handler)
+
+    backdoor_handler = CommandHandler('bd', backdoor)
+    application.add_handler(backdoor_handler)
 
     # xm and fire reaction handler
     # this handler must be put after all message handlers
