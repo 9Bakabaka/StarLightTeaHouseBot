da2b848cecada6f4238ddf9b67ad1bf9e4498ad3
Longtail <endermen359872@icloud.com>
2025-04-13 18:18:46 +0800

    Bugs fixed

--- main.py
+++ main.py
@@ -695,14 +695,20 @@ async def backdoor(update: Update, context: ContextTypes.DEFAULT_TYPE):
         print(datetime.datetime.now(), "\t", "Sending usage.")
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Format not correct.")
         return
-    # usage: /bd group_id message
+    # usage: /bd group_id message <reply_to message link>
     group_id = update.message.text.split(' ')[1]
-    message = update.message.text.split(' ')[2]
+    message = update.message.text.replace('/bd ' + group_id + ' ', '')
+    reply_to_msg_id = None
+    # if reply to message
+    if re.match(r'^.*https://t\.me/c/(\d+)/(\d+)$', message):
+        reply_to_msg_id = int(re.match(r'^.*https://t\.me/c/(\d+)/(\d+)$', message).group(2))
+        message = str(re.match(r'^(.*)https://t\.me/c/(\d+)/(\d+)$', message).group(1))
+    # send message
     try:
         print(datetime.datetime.now(), "\t", "Try sending ", message, " to ", group_id)
         send_to_chat = await context.bot.get_chat(chat_id=group_id)
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Try sending\n" + message + "\nto\n" + send_to_chat.title)
-        await context.bot.send_message(chat_id=send_to_chat.id, text=message)
+        await context.bot.send_message(chat_id=send_to_chat.id, text=message, reply_to_message_id=reply_to_msg_id)
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Success.")
     except Exception as e:
         print(datetime.datetime.now(), "\t", "Error: ", e)
