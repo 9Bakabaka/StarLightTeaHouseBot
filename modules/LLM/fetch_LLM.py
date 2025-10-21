4d8a8b0c9253fefb5b370e99db467c4c9b58e8dc
Longtail <endermen359872@icloud.com>
2025-10-21 11:24:03 +0800

    Refactored LLM module.

--- modules/LLM/fetch_LLM.py
+++ modules/LLM/fetch_LLM.py
@@ -1,54 +1,67 @@
-# This file is a copy of LLM.py in Must2502 SE252 project
 import os
+import datetime
 
 from openai import OpenAI, APIStatusError
 
-# with open('DSapi.txt', 'r') as f:
-#     DS_api = f.read()
-# with open('GPTapi.txt', 'r') as f:
-#     GPT_api = f.read()
 
-# platform: "DS" "GPT"
-def fetch_from_AI(platform, sys_prompt, user_prompt):
-    print("Start fetching from AI")
-    AI_model = ""
-    if platform == "DS":
-        base_url = "https://api.deepseek.com"
-        AI_model = "deepseek-chat"
-    elif platform == "GPT":
-        # base_url = "https://api.openai.com"
-        base_url = "https://api.f2gpt.com"
-        AI_model = "gpt-3.5-turbo"
-    else:
-        print("Error: Invalid platform")
-        return "Error"
+class LLM:
+    client = None
+    model = "deepseek-chat"  # Make it static for now
+    base_url = "https://api.deepseek.com"
+    messages = []
+    # [{"role": "system", "content": sys_prompt},
+    # {"role": "user", "content": user_prompt},
+    # {"role": "assistant", "content": "response"}]
+    stream = False
 
-    response = None
-    client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url=base_url)
-    try:
-        response = client.chat.completions.create(
-            model=AI_model,
-            messages=[
-                {"role": "system", "content": sys_prompt},
-                {"role": "user", "content": user_prompt},
-            ],
-            stream=False
-        )
-    except APIStatusError as e: # handle APIStatusError
-        if e.status_code == 402:
-            print("Insufficient Balance")
-        else:
-            print(f"APIStatusError: {e}")
-        return "Error"
+    def __init__(self, sys_prompt=None):
+        self.messages.append({"role": "system", "content": sys_prompt})
+        self.client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url=self.base_url)
 
-    except Exception as e:  # handle other exceptions
-        print(f"Error: {e}")
-        return "Error"
+    def add_message(self, role, content):
+        self.messages.append({"role": role, "content": content})
 
-    if response:
-        print("AI response: ", response.choices[0].message.content)
-        return response.choices[0].message.content
+    def get_payload(self):
+        return {
+            "model": self.model,
+            "messages": self.messages,
+            "stream": self.stream
+        }
 
-    return "Error"
+    def get_last_assistant_message(self):
+        for message in reversed(self.messages):
+            if message["role"] == "assistant":
+                return message["content"]
+        return None
 
-# if excepting model "remember context", user_prompt = last_user_prompt + this_user_prompt
+    # send payload would add the assistant message to messages
+    def send_payload(self):
+        print(datetime.datetime.now(), "\t", "[fetch_LLM.send_payload] send_payload called.")
+        response = None
+        try:
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=self.messages,
+                stream=self.stream
+            )
+            if not response:
+                print(datetime.datetime.now(), "\t", "[fetch_LLM.send_payload] Error: Response is None.")
+                return "Error"
+
+            print(datetime.datetime.now(), "\t", "[fetch_LLM.send_payload] response message:", response.choices[0].message.content)
+            content = response.choices[0].message.content
+            self.add_message("assistant", content)
+            return content
+        except APIStatusError as e:  # handle APIStatusError
+            if e.status_code == 402:
+                print(datetime.datetime.now(), "\t", "[fetch_LLM.send_payload] Error: Insufficient Balance")
+            else:
+                print(datetime.datetime.now(), "\t", f"[fetch_LLM.send_payload] Error: APIStatusError: {e}")
+            return "Error"
+
+    def multi_round_chat(self, user_prompt):
+        response = None
+        self.add_message("user", user_prompt)
+        # send payload
+        response = self.send_payload()
+        return self.get_last_assistant_message()
